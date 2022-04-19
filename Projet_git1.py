459a0c7c76c1360ffea7de3daf85b220e153d2bc
Meynahmed <102529101+Meynahmed@users.noreply.github.com>
2022-04-19 11:42:33 +0200

    Add files via upload

---
+++ Projet_git1.py
@@ -0,0 +1,6 @@
+def fonction(x, y):
+    g = x*y
+    print (" la valeur de g est ", g)
+x = float(input(" Entrez une valeur de x : "))
+y = float(input(" Entrez une valeur de y : "))
+fonction(x, y)
