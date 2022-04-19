4c104e84b3ac67ba8e29ab95d7eeaa4565ebbcd2
AminaELAKARI <amina.chaqchaq@gmail.com>
2022-04-19 11:55:50 +0200

    j'ai changé le produit en somme

--- Projet_git1.py
+++ Projet_git1.py
@@ -1,6 +1,6 @@
 def fonction(x, y):
-    g = x*y
+    g = x+y
     print (" la valeur de g est ", g)
 x = float(input(" Entrez une valeur de x : "))
-y = float(input(" Entrez une valeur de y : "))
+y = float(input("Entrez une valeur de y  : "))
 fonction(x, y)
