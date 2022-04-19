7ad04d04f54141265f4edf9680a1d34a1e21d63c
Azeddine211 <azeddine.alouani21@gmail.com>
2022-04-19 14:20:15 +0200

    Changement Azeddine

--- Projet_git1.py
+++ Projet_git1.py
@@ -1,5 +1,5 @@
 def fonction(x, y):
-    g = x*y
+    g = x*y = 0
     print (" la valeur de g est ", g)
 x = float(input(" Entrez une valeur de x : "))
 y = float(input(" Entrez une valeur de y : "))
